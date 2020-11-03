4f4bbbe37cb7594482a8def809f4d7c5c713dcca
Ekaterina Ivanova <eivanova@cs-cart.com>
2020-11-03 14:59:01 +0400

    use fib function in index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require __DIR__ . '/vendor/autoload.php';
 use Hexlet\Php\Runner;
 use function Hexlet\Php\Binary\binarySum;
 use function Hexlet\Php\PowerOfThree\isPowerOfThree;
+use function Hexlet\Php\Fib\fib;
 
 //print_r(Runner\run());
 
@@ -18,8 +19,16 @@ echo "\n";
 echo binarySum('1101', '101'); // 10010
 echo "\n"; */
 
-var_dump(isPowerOfThree(1)); // → true (3^0)
+/* var_dump(isPowerOfThree(1)); // → true (3^0)
 var_dump(isPowerOfThree(3)); // → true
 var_dump(isPowerOfThree(4)); // → false
 var_dump(isPowerOfThree(9)); // → true
-var_dump(isPowerOfThree(0)); // → false
+var_dump(isPowerOfThree(0)); // → false */
+
+var_dump(fib(3)); //2
+var_dump(fib(5)); //5
+var_dump(fib(10)); //55
+
+var_dump(fib(0)); //0
+var_dump(fib(1)); //1
+var_dump(fib(2)); //1
