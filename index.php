a2f11fa825036a797574ab2c31313d18792c48a0
Ekaterina Ivanova <eivanova@cs-cart.com>
2020-11-03 13:35:10 +0400

    use isPowerOfThree function in index.php

--- index.php
+++ index.php
@@ -8,11 +8,18 @@ require __DIR__ . '/vendor/autoload.php';
 // Он загрузится автоматически благодаря автозагрузке
 use Hexlet\Php\Runner;
 use function Hexlet\Php\Binary\binarySum;
+use function Hexlet\Php\PowerOfThree\isPowerOfThree;
 
 //print_r(Runner\run());
 
 
-echo binarySum('10', '1'); // 11
+/* echo binarySum('10', '1'); // 11
 echo "\n";
 echo binarySum('1101', '101'); // 10010
-echo "\n";
+echo "\n"; */
+
+var_dump(isPowerOfThree(1)); // → true (3^0)
+var_dump(isPowerOfThree(3)); // → true
+var_dump(isPowerOfThree(4)); // → false
+var_dump(isPowerOfThree(9)); // → true
+var_dump(isPowerOfThree(0)); // → false
