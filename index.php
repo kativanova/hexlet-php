b8678b710a8834b532f90869cb53f29c6273f53b
Ekaterina Ivanova <eivanova@cs-cart.com>
2020-11-03 13:14:30 +0400

    use binarySum function in index.php

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
-/** 
- * This file is used for exprtimenting with composer 
+/**
+ * This file is used for exprtimenting with composer
  */
 require __DIR__ . '/vendor/autoload.php';
 
 // Файл не включается на прямую
 // Он загрузится автоматически благодаря автозагрузке
 use Hexlet\Php\Runner;
+use function Hexlet\Php\Binary\binarySum;
 
-print_r(Runner\run());
+//print_r(Runner\run());
+
+
+echo binarySum('10', '1'); // 11
+echo "\n";
+echo binarySum('1101', '101'); // 10010
+echo "\n";
