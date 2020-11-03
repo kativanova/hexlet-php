e29fee04002e189d6f376b4a8588291ab33aed40
Ekaterina Ivanova <eivanova@cs-cart.com>
2020-11-03 13:33:57 +0400

    implements isPowerOfThree function

---
+++ src/PowerOfThree.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hexlet\Php\PowerOfThree;
+
+/**Определяет, является ли переданное число натуральной степенью тройки.
+ * Например, число 27 это третья степень (33), а 81 это четвертая (34).
+ * */
+
+function isPowerOfThree(int $number): bool
+{
+    
+    if ($number === 0) {
+        return false;
+    } elseif ($number === 1 || $number % 3 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
