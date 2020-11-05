c8c433c712b1c809a45aad66a19a7dc682bb722f
Ekaterina Ivanova <eivanova@cs-cart.com>
2020-11-05 15:01:56 +0400

    use fzzBuzz function in index.php

--- index.php
+++ index.php
@@ -6,10 +6,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 // Файл не включается на прямую
 // Он загрузится автоматически благодаря автозагрузке
-use function Hexlet\Php\Ticket\isHappy;
+use function Hexlet\Php\Solution\fizzBuzz;
 
-var_dump(isHappy('385916')); //true
-var_dump(isHappy('231002')); //false
-var_dump(isHappy('1222')); //false
-var_dump(isHappy('054702')); //true
-var_dump(isHappy('00')); //true
+fizzBuzz(11, 20);
