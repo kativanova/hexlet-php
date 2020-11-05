8b4e8ebb6c40c34f9ecd9b82d4df0091d179d7da
Ekaterina Ivanova <eivanova@cs-cart.com>
2020-11-05 12:18:16 +0400

    implements addDigits function

---
+++ src/AddDigits.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hexlet\Php\AddDigits;
+
+function addDigits(int $number): int
+{
+    $numberStr = (string) $number;
+    $strLenght = strlen($numberStr);
+
+    while ($strLenght > 1) {
+        $sum = 0;
+        for($i = 0; $i < $strLenght; $i++) {
+            $sum += (int) $numberStr[$i];
+        }
+        $numberStr = (string) $sum;
+        $strLenght = strlen($numberStr);
+    }
+    return (int) $numberStr;
+}
