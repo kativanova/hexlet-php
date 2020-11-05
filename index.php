d3ed0a2fc81a86947e4073eae1673f9f602d2a9d
Ekaterina Ivanova <eivanova@cs-cart.com>
2020-11-05 12:22:09 +0400

    use addDigits function in index.php

--- index.php
+++ index.php
@@ -6,29 +6,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 // Файл не включается на прямую
 // Он загрузится автоматически благодаря автозагрузке
-use Hexlet\Php\Runner;
-use function Hexlet\Php\Binary\binarySum;
-use function Hexlet\Php\PowerOfThree\isPowerOfThree;
-use function Hexlet\Php\Fib\fib;
+use function Hexlet\Php\AddDigits\addDigits;
 
-//print_r(Runner\run());
-
-
-/* echo binarySum('10', '1'); // 11
-echo "\n";
-echo binarySum('1101', '101'); // 10010
-echo "\n"; */
-
-/* var_dump(isPowerOfThree(1)); // → true (3^0)
-var_dump(isPowerOfThree(3)); // → true
-var_dump(isPowerOfThree(4)); // → false
-var_dump(isPowerOfThree(9)); // → true
-var_dump(isPowerOfThree(0)); // → false */
-
-var_dump(fib(3)); //2
-var_dump(fib(5)); //5
-var_dump(fib(10)); //55
-
-var_dump(fib(0)); //0
-var_dump(fib(1)); //1
-var_dump(fib(2)); //1
+var_dump(addDigits(0)); // 0
+var_dump(addDigits(919)); // 1
